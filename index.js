3f47126a43eb25967170dad0fa37aa6d70345c71
Yael Marmon <ymarmon@gmail.com>
2017-11-01 20:35:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log('HELLO')
-  return('hello')
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log('hello')
-  return('hello')
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string){
-  return ('I can\'t hear you!')
+  if (string.toLowerCase() === string){
+    return "I can\'t hear you!"
+  } else if (string.toUpperCase) === string {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you too."
+  } 
 }
